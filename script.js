bb25b79db622a7206a6056ccaec2b95d8b4c9476
bida23-080 <159456350+bida23-080@users.noreply.github.com>
2024-05-12 17:15:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    
     // Get all the interactive items (TV show titles)
     const interactiveItems = document.querySelectorAll('.interactive-item');
 
